596332551e3f4846c91a63ddf54ac33f48001210
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-05-31 23:25:13 +0300

    ab

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-
-
 def remove_char_at(str, n):
-	if n < 0:
-		return str
-	return (str[0:n] + str[n + 1:])
+    if n < 0:
+        return str
+    count = 0
+    str_copy = ""
+    for element in str:
+        if count == n:
+            count += 1
+            continue
+        str_copy += str[count]
+        count += 1
+    return str_copy
+	 
--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
+#!/bin/python3
 
 
 def magic_calculation(a, b, c):
-	if a < b:
-		return c
-	if c > b:
-		return a + b
-	return a * b - c
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    return (a * b) - c
+	 
