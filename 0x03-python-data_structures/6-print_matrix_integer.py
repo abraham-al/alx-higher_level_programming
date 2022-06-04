ea708cba19ee55a267be2d308e1462762375c5fa
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-06-04 14:56:41 +0300

    Tuples addition

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    l = len(matrix)
-    for x in range(l):
+    for x in range(len(matrix)):
         for y in range(len(matrix[x])):
             print('{:d}'.format(matrix[x][y]), end='')
             if y is not (len(matrix[x]) - 1):
