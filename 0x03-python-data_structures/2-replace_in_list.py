330c853dd780c00dc2d12894d0300e53daebb8f1
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-06-04 13:34:09 +0300

    Replace element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return (None)
+    elif idx > len(my_list) - 1:
+        return (None)
+    else:
+        my_list[idx] = element
+        return (my_list)
