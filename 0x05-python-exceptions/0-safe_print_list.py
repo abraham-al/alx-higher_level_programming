3fd13efe8120427a9dc44ccbfa1570549271b078
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-06-20 21:12:53 +0300

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    j = 0
+    for i in range(0,x):
+        try:
+            print(f"{my_list[i]}", end="")
+            j += 1
+        except IndexError:
+            continue
+    print()
+    return int(j)
