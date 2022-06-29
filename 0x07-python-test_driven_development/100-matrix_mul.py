b6d258d2087979c64b321d090c2794ea7c6af5c9
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-06-29 09:03:07 +0300

    Matrix mul

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -15,7 +15,6 @@ def matrix_mul(m_a, m_b):
     Returns:
         A new matrix representing the multiplication of m_a by m_b.
     """
-
     if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
     if m_b == [] or m_b == [[]]:
