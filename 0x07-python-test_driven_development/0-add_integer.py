f37c22f3564973d7ff66c9362677aca610177ebc
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-06-29 08:49:57 +0300

    Add integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,12 +3,8 @@
 
 
 def add_integer(a, b=98):
-    """ Return the integer addition of a and b.float arg are typecasted to int before addition
-
-    Args:
-        a (int): first arg
-        b (int, optional): second arg. Defaults to 98.
-    
+    """Return the integer addition of a and b.
+    Float arguments are typecasted to ints before addition is performed.
     Raises:
         TypeError: If either of a or b is a non-integer and non-float.
     """
