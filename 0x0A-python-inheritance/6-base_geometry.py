ab06c6e7c03591c3f927cd7c4a7c95a4491f58fa
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-07-04 17:50:22 +0300

    a class BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    BaseGeometry class
+"""
+
+
+class BaseGeometry():
+    """
+        BaseGeometry Class
+    """
+    def area(self):
+        """
+            public instance
+            Raise:
+                Exception: Area is not implemented
+        """
+        raise Exception("area() is not implemented")
