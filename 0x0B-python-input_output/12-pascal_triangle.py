1b747f425fedadc8515df597591d0fb62bfce41f
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-07-05 22:03:28 +0300

    Pascal Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""
-Defines pasacal triangle that create list of lists
-"""
+""" pascal triangle"""
 
 
-def pascal_triangle(n):
-    """
-    defines triangle module
-    """
-    if n <= 0:
-        return []
-    temp = []
-    l = []
+def pascal_triangle(n=4500):
+    """print pascal"""
+    pascal = [[0]*i for i in range(1, n+1)]
+    cmpt = 0
     for i in range(n):
-        row = []
-        for j in range(i + 1):
-            if i == 0 or j == 0 or i == j:
-                row.append(1)
-            else:
-                row.append(l[j] + l[j - 1])
-        l = row
-        temp.append(row)
-    return temp
+        pascal[i][0] = 1
+        pascal[i][-1] = 1
+        for j in range(0, i//2):
+            pascal[i][j+1] = pascal[i-1][j] + pascal[i-1][j+1]
+            pascal[i][i-j-1] = pascal[i-1][j] + pascal[i-1][j+1]
+
+    return pascal
