67e7e76b4a036ea7c05288f87846ce2d382ab81f
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-09-05 18:43:48 +0300

    a little fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-function Big (arr) {
-    let maxnum = 0; let result = 0;
-    for (const value of arr) {
-      const nr = Number(value);
-      if (nr > maxnum) {
-        [result, maxnum] = [maxnum, nr];
-      } else if (nr < maxnum && nr > result) {
-        result = nr;
-      }
-    }
-    return (result);
+let args = process.argv.slice(2).map((x) => {
+    return parseInt(x);
+  });
+  
+  if (args.length <= 1) {
+    console.log(0);
+  } else {
+    console.log(args.sort((a, b) => {
+      return b - a;
+    })[1]);
   }
-  console.log(Big(process.argv));
