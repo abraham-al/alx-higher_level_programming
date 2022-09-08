dbc9f0b8e6d747f96db87dfd361a4c9aec97758e
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-09-08 15:09:32 +0300

    Sorted occurences

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const di = {};
+for (const key in dict) {
+  if (di[dict[key]] === undefined) {
+    di[dict[key]] = [];
+  }
+  di[dict[key]].push(key);
+}
+console.log(di);
