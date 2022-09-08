cb7a2088ae0b2cd0043a97b63abb1ed3011146a6
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-09-08 12:18:13 +0300

    square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const SquareModel = require('./5-square.js');
+
+module.exports = class Square extends SquareModel {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined || c == null || c.length <= 0) {
+      this.print();
+    } else {
+      let wid = '';
+      for (let i = 0; i < this.width; i++) {
+        wid += c;
+      }
+      for (let i = 0; i < this.height; i++) {
+        console.log(wid);
+      }
+    }
+  }
+};
