189a655267ee55169a5ba17bd2e2b6dba647ace5
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-09-13 18:28:09 +0300

    All submition

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database """
-import MySQLdb
+"""List all states from a given db sorted in ascending order by id
+Username, password, and database names are given as user args
+"""
 import sys
+import MySQLdb
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+    allStates = cur.fetchall()
 
-    db = MySQLdb.connect(
-        user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3],
-        host="localhost",
-        port=3306)
+    for state in allStates:
+        print(state)
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
-    for row in cursor.fetchall():
-        print(row)
-    cursor.close()
+    cur.close()
     db.close()
---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+creates the State “California” with the City “San Francisco”
+from the database hbtn_0e_100_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(
+                                        sys.argv[1],
+                                        sys.argv[2],
+                                        sys.argv[3]
+                                            ),
+                            pool_pre_ping=True
+                                )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_State = State(name="California")
+    new_State.cities = [City(name="San Francisco")]
+    re = session.add(new_State)
+    session.commit()
+    session.close()
