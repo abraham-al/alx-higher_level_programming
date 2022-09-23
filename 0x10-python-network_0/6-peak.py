d82e10cf625a5ef335963c9feb6c2d9ab912ee55
abraham-al <abraham.alemseged.f@gmail.com>
2022-09-23 06:26:48 -0700

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" function that finds a peak in a list of unsorted integers """
+
+
+def find_peak(list_of_integers):
+        """function to find the peak"""
+        if list_of_integers:
+            list_of_integers.sort()
+            return list_of_integers[-1]
+        else:
+            return None     
