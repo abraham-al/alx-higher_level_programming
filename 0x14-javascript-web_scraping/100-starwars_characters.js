080c74fcc0c12daf9aba47332e1729a0d1dfcc85
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-10-25 11:33:05 +0300

    JD

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,21 @@
 #!/usr/bin/node
 
-const request = require('request');
-
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+req.get(url + id, function (error, res, body) {
   if (error) {
-    console.error(error);
+    console.log(error);
   }
-  JSON.parse(body).characters.forEach(function (url, callback) {
-    request(url, function (error, response, body) {
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
       if (error) {
-        console.error(error);
+        console.log(error);
       }
-      console.log(JSON.parse(body).name);
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
     });
-  });
+  }
 });
